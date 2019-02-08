ea92e9d450451e88fdec292808e23a2a4717ded8
JatraMaya <kirahimura@gmail.com>
2019-02-08 16:46:24 +0800

    Another Overhaul

--- script/main.js
+++ script/main.js
@@ -1,39 +1,42 @@
-$(document).ready(function() {
+// Lot's of variables
 
-    let scrollitem = $('.scroll');
+const body = document.getElementsByTagName('body')[0];
+const burger = document.getElementsByClassName('burger')[0];
+const nav = document.getElementsByClassName('main-nav')[0];
+const overlay = document.getElementsByClassName('overlay')[0];
+const overlayMenu = overlay.querySelectorAll('a');
+const section = document.querySelectorAll('section');
 
-    let burger = $('.hamburger');
 
-    scrollitem.click(function(e){
-        e.preventDefault();
-        $('body, html').animate({
-            scrollTop: $(this.hash).offset().top
-        }, 500);
-    })
-
-    burger.click(function(){
-        burger.toggleClass('is-active');
-        $('.overlay').toggleClass('overlay-on');
-        $('section').toggleClass('is-blur');
-    })
-
-    $('.mobile-menu').click(function(){
-        burger.toggleClass('is-active');
-        $('.overlay').toggleClass('overlay-on');
-        $('section').toggleClass('is-blur'); 
-    })
-
+burger.addEventListener('click', ()=>{
+    burger.classList.toggle('burger-active');
+    overlay.classList.toggle('overlay-active');
+    body.classList.toggle('overflow-stop');
+    for (let i = 0; i < section.length; i ++){
+      section[i].classList.toggle('is-blur');
+    }
 })
 
+for (let i = 0; i < overlayMenu.length; i++){
+    overlayMenu[i].addEventListener('click',()=>{
+        burger.classList.toggle('burger-active');
+        overlay.classList.toggle('overlay-active');
+        body.classList.toggle('overflow-stop');
+        for (let i = 0; i < section.length; i ++){
+          section[i].classList.toggle('is-blur');
+        }  
+    })
+}
+
 let prevScrollpos = window.pageYOffset;
-window.onscroll = ()=> {
-  let currentScrollPos = window.pageYOffset;
+window.onscroll = () => {
+  var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
-    document.getElementsByTagName('nav')[0].style.top = '0';
-    document.getElementsByClassName('hamburger')[0].style.top = '0';
+    burger.style.top = "0";
+    nav.style.top = "0";
   } else {
-    document.getElementsByTagName('nav')[0] .style.top = '-50px';
-    document.getElementsByClassName('hamburger')[0].style.top = '-50px';
+    burger.style.top = "-60px";
+    nav.style.top = "-55px";
   }
   prevScrollpos = currentScrollPos;
 } 
