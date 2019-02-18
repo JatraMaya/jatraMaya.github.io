0e48211959b5339a20db65cfbef60c73f04b010a
JatraMaya <kirahimura@gmail.com>
2019-02-18 22:16:47 +0800

    add desktop media query

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 // List of Variables
 
+const nav = document.getElementsByClassName('main-nav')[0];
 const burger = document.getElementsByClassName('burger')[0];
 const overlay = document.getElementsByClassName('overlay')[0];
 const section = document.getElementsByTagName('section');
@@ -9,11 +10,21 @@ let scrollPos = window.pageYOffset;
 window.onscroll = () => {
   let posNow = window.pageYOffset;
 
+  if (posNow > nav.clientHeight) {
+    nav.style.background = '#e3d4bf'
+  } else {
+    nav.style.transition = 'background .6s ease-in'
+    nav.style.background = 'transparent'
+  }
+
   if (scrollPos > posNow) {
     burger.style.top = '2%';
+    nav.style.top = '0';
    } else {
+    nav.style.top = '-50%';
     burger.style.top = '-50%';
     burger.style.transition = 'top .5s ease';
+    nav.style.transition = 'top .5s ease';
   } scrollPos = posNow;
 }
 
